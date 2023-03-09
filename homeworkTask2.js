a5fc44457399a63c0bc7170d35d34221a883c17c
Oleg Kudelkin <tryhard.work@mail.ru>
2023-03-09 05:53:20 +0300

    modify homeworkTask2

--- homeworkTask2.js
+++ homeworkTask2.js
@@ -7,11 +7,15 @@
 // Первый способ. Вручную написать алгоритм.
 const arr = [5, 1, 3, 2, -15, 4, 0, 152, -1, -2]
 
-function sortArr(arr) {
+function callbackSort(a, b) {
+	return a > b
+}
+
+function sortArr(arr, callback) {
 	let sortedArray = [...arr]
 	for(let i = 0; i < sortedArray.length; i++) {
 		for(let j = 0; j < sortedArray.length - 1; j++) {
-			if(sortedArray[j] > sortedArray[j + 1]) {
+			if(callback(sortedArray[j], sortedArray[j + 1])) {
 				let temp = sortedArray[j]
 				sortedArray[j] = sortedArray[j + 1]
 				sortedArray[j + 1] = temp
@@ -21,7 +25,7 @@ function sortArr(arr) {
 	return sortedArray
 }
 
-// console.log(sortArr(arr))
+console.log(sortArr(arr, callbackSort))
 
 
 // Второй способ. Через метод массива sort()
